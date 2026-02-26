d3acd4c868bad6cef38be268f1bc5dabe0b34d5d
Ctobinson C <calebthowinson@hotmail.com>
2026-02-26 11:58:45 -0500

    pipeline_worcunt

---
+++ dataflow/pipeline_wordcount.py
@@ -0,0 +1,24 @@
+import apache_beam as beam
+from apache_beam.options.pipeline_options import PipelineOptions
+
+def run():
+    # Configuración del pipeline
+    options = PipelineOptions(
+        runner="DataflowRunner",  # Cambiar a DirectRunner para local
+        project="gcp-data-engineer-curso-04",
+        region="us-central1",
+        temp_location="gs://gcs-bucket-pipeline-ct/temp"
+    )
+
+    with beam.Pipeline(options=options) as p:
+        (
+            p
+            | "Leer archivo" >> beam.io.ReadFromText("gs://dataflow-samples/shakespeare/kinglear.txt")
+            | "Separar palabras" >> beam.FlatMap(lambda line: line.split())
+            | "Contar palabras" >> beam.combiners.Count.PerElement()
+            | "Guardar resultados" >> beam.io.WriteToText("gs://gcs-bucket-pipeline-ct/output/wordcount")
+        )
+    print("Pipeline ejecutado exitosamente.")
+
+if __name__ == "__main__":
+    run()
