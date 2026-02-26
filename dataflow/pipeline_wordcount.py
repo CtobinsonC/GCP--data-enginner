57af8e1315d86d8c753f4bdeaf9b28839c64f760
Ctobinson C <calebthowinson@hotmail.com>
2026-02-26 16:35:11 -0500

    coreccion ambiente virtual

--- dataflow/pipeline_wordcount.py
+++ dataflow/pipeline_wordcount.py
@@ -5,9 +5,10 @@ def run():
     # Configuración del pipeline
     options = PipelineOptions(
         runner="DataflowRunner",  # Cambiar a DirectRunner para local
-        project="gcp-data-engineer-curso-04",
+        project="gcp-data-enginner-pipeline-ct",
         region="us-central1",
-        temp_location="gs://gcs-bucket-pipeline-ct/temp"
+        temp_location="gs://gcs-bucket-pipeline-ctc/temp",
+        staging_location="gs://gcs-bucket-pipeline-ctc/staging"
     )
 
     with beam.Pipeline(options=options) as p:
@@ -16,7 +17,7 @@ def run():
             | "Leer archivo" >> beam.io.ReadFromText("gs://dataflow-samples/shakespeare/kinglear.txt")
             | "Separar palabras" >> beam.FlatMap(lambda line: line.split())
             | "Contar palabras" >> beam.combiners.Count.PerElement()
-            | "Guardar resultados" >> beam.io.WriteToText("gs://gcs-bucket-pipeline-ct/output/wordcount")
+            | "Guardar resultados" >> beam.io.WriteToText("gs://gcs-bucket-pipeline-ctc/output/wordcount")
         )
     print("Pipeline ejecutado exitosamente.")
 
