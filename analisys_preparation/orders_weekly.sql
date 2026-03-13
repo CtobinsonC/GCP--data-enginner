fbdfccf649dbf9835f82883a3a7a0210f2e4ad4a
Ctobinson C <calebthowinson@hotmail.com>
2026-03-12 19:55:10 -0500

    preparacion de datos bigquery

---
+++ analisys_preparation/orders_weekly.sql
@@ -0,0 +1,8 @@
+CREATE OR REPLACE VIEW ecommerce_analysis.orders_weekly AS
+SELECT
+    FORMAT_DATE('%Y-%W', order_date) AS order_week,
+    COUNT(order_id) AS total_orders,
+    SUM(price) AS total_revenue
+FROM ecommerce_analysis.orders_raw
+GROUP BY order_week
+ORDER BY order_week;
